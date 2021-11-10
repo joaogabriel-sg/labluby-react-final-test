1ea88770a54aadcb7c51a2242dae0b489418dc04
João Gabriel <main.joaogabrielsg@gmail.com>
2021-11-10 15:06:37 -0300

    feat: add global style and theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { ThemeProvider } from "styled-components";
+
+import { GlobalStyle, theme } from "./styles";
+
 export function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <ThemeProvider theme={theme}>
+      <h1>Hello World!</h1>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
---
+++ src/styles/theme.ts
@@ -0,0 +1,19 @@
+export const theme = {
+  fonts: ["Helvetica", "Arial", "sans-serif"].join(", "),
+  colors: {
+    background: "#f7f7f7",
+    primary: "#b5c401",
+    white: "#ffffff",
+    black: "#191919",
+    border: {
+      light: "#ebebeb",
+      dark: "#dddddd",
+    },
+    text: {
+      "100": "#c1c1c1",
+      "500": "#9d9d9d",
+      "600": "#868686",
+      "700": "#707070",
+    },
+  },
+};
