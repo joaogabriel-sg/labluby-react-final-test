eb536a4331347e87a3c233aeb49a9a11c512b4aa
João Gabriel <main.joaogabrielsg@gmail.com>
2021-11-15 14:35:24 -0300

    feat: add routes authentication

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,26 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import { LoginPage, HomePage, NewBetPage } from "../pages";
 
+import { useAppSelector } from "../hooks";
+
 export function AppRoutes() {
+  const isAuthenticated = useAppSelector((state) => state.auth.isAuthenticated);
+
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="login/*" element={<LoginPage />} />
-      <Route path="new-bet" element={<NewBetPage />} />
+      <Route
+        path="/"
+        element={isAuthenticated ? <HomePage /> : <Navigate to="/login" />}
+      />
+      <Route
+        path="login/*"
+        element={!isAuthenticated ? <LoginPage /> : <Navigate to="/" />}
+      />
+      <Route
+        path="new-bet"
+        element={isAuthenticated ? <NewBetPage /> : <Navigate to="/login" />}
+      />
     </Routes>
   );
 }
