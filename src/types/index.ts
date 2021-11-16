c118d600f4a45578e705059e7bd6625bfe34398b
João Gabriel <main.joaogabrielsg@gmail.com>
2021-11-15 21:29:40 -0300

    feat: add type of games type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,3 @@
 export * from "./bet";
 export * from "./user";
+export * from "./typeOfGame";
---
+++ src/types/typeOfGame.ts
@@ -0,0 +1,8 @@
+export type TypeOfGame = {
+  type: string;
+  description: string;
+  range: number;
+  price: number;
+  "max-number": number;
+  color: string;
+};
