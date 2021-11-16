34bcd11cd19601b29d561540281bdb52ac394aaa
João Gabriel <main.joaogabrielsg@gmail.com>
2021-11-16 14:59:51 -0300

    feat: add loading slice/reducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,12 +3,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import { authReducer } from "./auth";
 import { cartReducer } from "./cart";
 import { gamesReducer } from "./games";
+import { loadingReducer } from "./loading";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     cart: cartReducer,
     games: gamesReducer,
+    loading: loadingReducer,
   },
 });
 
